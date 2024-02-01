762b461e8c4ccc612dbd4fe4811028ac35eecce9
Raymond Camden <raymondcamden@gmail.com>
2024-02-01 09:04:05 -0600

    initial

---
+++ text_to_image.py
@@ -0,0 +1,53 @@
+import os 
+import requests 
+import json
+from slugify import slugify
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+
+def getAccessToken(id, secret):
+	response = requests.post(f"https://ims-na1.adobelogin.com/ims/token/v3?client_id={id}&client_secret={secret}&grant_type=client_credentials&scope=openid,AdobeID,firefly_enterprise,firefly_api")
+	return response.json()
+
+def textToImage(text, id, token):
+
+	data = {
+		"n":3,
+		"prompt":text,
+		"contentClass":"photo",
+		"size":{
+			"width":2048,
+			"height":2048
+		}
+	}
+
+	response = requests.post("https://firefly-beta.adobe.io/v2/images/generate", json=data, headers = {
+		"X-API-Key":id, 
+		"Authorization":f"Bearer {token}",
+		"Content-Type":"application/json"
+	}) 
+
+	return response.json()
+
+
+accessToken = getAccessToken(CLIENT_ID, CLIENT_SECRET)['access_token'];
+#print(accessToken)
+
+prompt = "cats on unicorns under a rainbow"
+
+response = textToImage(prompt, CLIENT_ID, accessToken)
+#print(json.dumps(response,indent=1))
+
+# So, assume a good response, and loop over response.outputs
+
+for resp in response["outputs"]:
+	# todo, make new file based on slug of prompt + seed
+	newName = slugify(prompt) + "-" + str(resp["seed"]) + ".jpg"
+	imgUrl = resp["image"]["presignedUrl"]
+	print(f"Saving {newName}")
+	with open(newName,'wb') as output:
+		bits = requests.get(imgUrl, stream=True).content
+		output.write(bits)
+
+print("\nDone")
---
+++ text_to_image_dynamic_prompt.py
@@ -0,0 +1,59 @@
+import os 
+import requests 
+import json
+import sys
+from slugify import slugify
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+
+def getAccessToken(id, secret):
+	response = requests.post(f"https://ims-na1.adobelogin.com/ims/token/v3?client_id={id}&client_secret={secret}&grant_type=client_credentials&scope=openid,AdobeID,firefly_enterprise,firefly_api")
+	return response.json()
+
+def textToImage(text, id, token):
+
+	data = {
+		"n":3,
+		"prompt":text,
+		"contentClass":"photo",
+		"size":{
+			"width":2048,
+			"height":2048
+		}
+	}
+
+	response = requests.post("https://firefly-beta.adobe.io/v2/images/generate", json=data, headers = {
+		"X-API-Key":id, 
+		"Authorization":f"Bearer {token}",
+		"Content-Type":"application/json"
+	}) 
+
+	return response.json()
+
+if len(sys.argv) < 2:
+	print("Usage: python3 test2.py \"prompt\"")
+	sys.exit()
+
+prompt = sys.argv[1]
+
+print(f"Generating images based on prompt: {prompt}")
+
+accessToken = getAccessToken(CLIENT_ID, CLIENT_SECRET)['access_token'];
+
+
+response = textToImage(prompt, CLIENT_ID, accessToken)
+#print(json.dumps(response,indent=1))
+
+# So, assume a good response, and loop over response.outputs
+
+for resp in response["outputs"]:
+	# todo, make new file based on slug of prompt + seed
+	newName = slugify(prompt) + "-" + str(resp["seed"]) + ".jpg"
+	imgUrl = resp["image"]["presignedUrl"]
+	print(f"Saving {newName}")
+	with open(newName,'wb') as output:
+		bits = requests.get(imgUrl, stream=True).content
+		output.write(bits)
+
+print("\nDone")
